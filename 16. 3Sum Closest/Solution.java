b82e4874be454057a51f3858dd3fa138b5eaff88
AmanVW <amanwalke97@gmail.com>
2023-03-03 23:49:17 +0530

    16

--- 16. 3Sum Closest/Solution.java
+++ 16. 3Sum Closest/Solution.java
@@ -1,17 +1,42 @@
+import java.lang.Math;
+import java.util.*;
 public class Solution {
   public static void main (String[] args) {
     Solution solution = new Solution();
 
-    int[] nums = new int[] {-1,2,1,-4};
-    int target = 1;
+    int[] nums = new int[] {1,1,1,0};
+    int target = -100;
 
     System.out.println(solution.threeSumClosest(nums, target));
 
   }
 
-  public int threeSumClosest(int[] nums, int[] target) {
+  public int threeSumClosest(int[] nums, int target) {
     Arrays.sort(nums);
+    int closest = 0;
+    int minDiff = Integer.MAX_VALUE;
 
-    // -4 -1 1 2
+    for (int i = 0; i < nums.length - 2; i++) {
+      int left = i + 1;
+      int right = nums.length - 1;
+
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        int diff = Math.abs(sum - target);
+
+        if (diff < minDiff) {
+          closest = sum;
+          minDiff = diff;
+        }
+
+        if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+
+      }
+    }
+    return closest;
   }
 }
